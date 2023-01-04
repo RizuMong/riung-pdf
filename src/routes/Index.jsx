ef6c0b0168ff9181fa7ec46c1979c0c1f1f498a8
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2023-01-04 16:02:50 +0700

    update routing and add endpoint

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -2,14 +2,49 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import ExcavatorControl from "../pages/ExcavatorControl";
-import DaftarPeriksaAlatAngkut from "../pages/DaftarPeriksaAlatAngkut"
+import DaftarPeriksaAlatAngkut from "../pages/DaftarPeriksaAlatAngkut";
+import HandOverProductionReport from "../pages/ProductionReport";
+import ControlActivityCoalGetting from "../pages/ControlActivityCoalGetting";
+import DaftarPeriksaAlatLoading from "../pages/DaftarPeriksaAlatLoading";
+import ProductionControl from "../pages/ProductControl";
+import HandoverPitSupportServiceActivity from "../pages/ReportPitService";
+import RoadMaintenanceHandover from "../pages/RoadMaintenanceHandover";
+import DaftarPeriksaLeader from "../pages/DaftarPeriksaLeader";
 
 const Index = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<ExcavatorControl />} />
-        <Route path="/periksa-alat-angkut" element={<DaftarPeriksaAlatAngkut />} />
+        <Route
+          path="/periksa-alat-angkut"
+          element={<DaftarPeriksaAlatAngkut />}
+        />
+        <Route
+          path="/production-report"
+          element={<HandOverProductionReport />}
+        />
+        <Route
+          path="/daftar-periksa-alat-loading"
+          element={<DaftarPeriksaAlatLoading />}
+        />
+        <Route path="/product-control" element={<ProductionControl />} />
+        <Route
+          path="/report-pit-service"
+          element={<HandoverPitSupportServiceActivity />}
+        />
+        <Route
+          path="/control-activity-coal-getting"
+          element={<ControlActivityCoalGetting />}
+        />
+        <Route
+          path="/daftar-periksa-leader"
+          element={<DaftarPeriksaLeader />}
+        />
+        <Route
+          path="/road-maintenance-handover"
+          element={<RoadMaintenanceHandover />}
+        />
       </Routes>
     </BrowserRouter>
   );
