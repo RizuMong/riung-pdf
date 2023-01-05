3a18f665470c7640e26b67cca75c9ee5d833463e
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2023-01-05 14:39:34 +0700

    update

--- src/pages/ProductControl.jsx
+++ src/pages/ProductControl.jsx
@@ -9,7 +9,7 @@ const ProductControl = () => {
   return (
     <div className="container px-5 pb-5">
       <div className="mt-2">
-        <p className="text-end fst-italic">FRM – OPR - 002</p>
+        <p className="text-end fst-italic">FRM – OPR - 004</p>
         <div className="border border-2 border-dark">
           <div className="p-3">
             {/* Header */}
@@ -53,10 +53,290 @@ const ProductControl = () => {
               </thead>
             </table>
 
+            <hr className="w-100 mb-3 mt-2 solid" />
             {/* Content Table */}
+            <Table responsive striped bordered hover>
+              <thead className="text-center">
+                <tr>
+                  <th
+                    rowSpan={2}
+                    scope="col-3"
+                    className="header-table-product"
+                    style={{ verticalAlign: "middle" }}
+                  >
+                    Waktu
+                  </th>
+                  <th
+                    scope="col-3"
+                    colSpan={2}
+                    className="header-table-product"
+                  >
+                    Produksi
+                  </th>
+                  <th
+                    scope="col-3"
+                    colSpan={2}
+                    className="header-table-product"
+                  >
+                    Akumulasi Produksi
+                  </th>
+                  <th
+                    rowSpan={2}
+                    scope="col-3"
+                    className="header-table-product"
+                    style={{ verticalAlign: "middle" }}
+                  >
+                    Keterangan
+                  </th>
+                </tr>
+                <tr>
+                  <th scope="col-3">Plan</th>
+                  <th scope="col-3">Aktual</th>
+                  <th scope="col-3">Plan</th>
+                  <th scope="col-3">Aktual</th>
+                </tr>
+              </thead>
+              <tbody className="text-center">
+                <tr>
+                  <td>07:00 - 08:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>07:00 - 08:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>08:00 - 09:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>09:00 - 10:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>10:00 - 11:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>11:00 - 12:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>12:00 - 13:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>13:00 - 14:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>14:00 - 15:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>15:00 - 16:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>16:00 - 17:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>17:00 - 18:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>18:00 - 19:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>19:00 - 20:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>20:00 - 21:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>21:00 - 22:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>22:00 - 23:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>23:00 - 24:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>00:00 - 01:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>01:00 - 02:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>02:00 - 03:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>03:00 - 04:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>04:00 - 05:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>05:00 - 06:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>06:00 - 07:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </Table>
 
             {/* Content Bottom */}
-            
+            <table class="table mt-3">
+              <thead className="text-center">
+                <tr>
+                  <th scope="col" className="text-serif fw-bold">
+                    Shift
+                  </th>
+                  <th scope="col" className="text-serif fw-bold">
+                    Section Head
+                  </th>
+                  <th scope="col" className="text-serif fw-bold">
+                    Tanda Tangan
+                  </th>
+                  <th scope="col" className="text-serif fw-bold">
+                    Catatan{" "}
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="text-center">
+                <tr>
+                  <th className="text-serif" scope="row">
+                    I
+                  </th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <th className="text-serif" scope="row">
+                    II
+                  </th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
