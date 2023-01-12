a627ededbfb09b7fee9dbae9af8182873bdd2f29
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2023-01-12 17:43:27 +0700

    update

--- src/pages/pdf-2/DrillDesign.jsx
+++ src/pages/pdf-2/DrillDesign.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { WithRouter } from "../../utils/Navigation";
 
+import Img_Sketsa from "../../assets/img_sketsa.jpg"
+import Img_TTD from "../../assets/img_ttd.png"
 import LogoRiung from "../../assets/logo-riung.jpg";
 import "../../styles/App.css";
 
@@ -51,6 +53,64 @@ const DrillDesign = () => {
                   </th>
                 </thead>
               </table>
+
+              <div className="table-responsive">
+                <table className="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th style={{width: "30%"}}>
+                        <h5 className="fs-6 fw-semibold p-1">LOKASI <span className="fst-italic">DRILLING</span></h5>
+                        <div className="p-1 fs-6 fw-normal">
+                          <p className="px-3">PIT: EAST KAWI</p>
+                          <p className="px-3">BLOK: -</p>
+                          <p className="px-3">STRIP: -</p>
+                          <p className="px-3">ELEV.: 183</p>
+                          <p className="px-3">RL: 175</p>
+                        </div>
+                      </th>
+                      <th rowSpan={3} style={{verticalAlign: "top", width: "60%"}} className="fw-semibold">
+                        <h5 className="fs-6 p-1 fw-semibold">SKETSA</h5>
+                        <img src={Img_Sketsa} alt="Gambar Sketsa" width="1000px" />
+                      </th>
+                    </tr>
+                    <tr>
+                    <th style={{width: "30%"}}>
+                        <h5 className="fs-6 fw-semibold p-1">DIMENSI <span className="fst-italic">DRILLING</span></h5>
+                        <div className="p-1 fs-6 fw-normal">
+                          <p className="px-3">BURDEN: 6 (m)</p>
+                          <p className="px-3">SPACING : 7 (m)</p>
+                          <p className="px-3">HOLE DIAMETER : 6 3/4 (inch)</p>
+                          <p className="px-3">HOLE DEPTH : 8 (m)</p>
+                          <p className="px-3">STEMMING : 3.5 (m)</p>
+                          <p className="px-3">SUBDRILL : 0.5 (m)</p>
+                          <p className="px-3">INCLINATION : - (derajat)</p>
+                          <p className="px-3">PATTERN : Echelon</p>
+                          <p className="px-3">ROW : 4</p>
+                          <p className="px-3">No. Of Hole : 70</p>
+                          <p className="px-3">VOLUME : 23.520 (Bcm)</p>
+                        </div>
+                        <h5 className="fs-6 fw-semibold p-1">UNIT C/N: DR 103</h5>
+                      </th>
+                    </tr>
+                    <tr>
+                      <th className="text-center d-flex justify-content-evenly gap-5">
+                        <div className="mt-5 mb-5">
+                          <p className="fw-normal">Dibuat Oleh,</p>
+                          <img src={Img_TTD} alt="TTD" width="100px"/>
+                          <p className="fw-normal">(M. Arif)</p>
+                          <p>Drill & Blast Eng</p>
+                        </div>
+                        <div className="mb-5 mt-5">
+                          <p className="fw-normal">Dibuat Oleh,</p>
+                          <img src={Img_TTD} alt="TTD" width="100px"/>
+                          <p className="fw-normal">(M. Arif)</p>
+                          <p>Drill & Blast Eng</p>
+                        </div>
+                      </th>
+                    </tr>
+                  </thead>
+                </table>
+              </div>
             </div>
             <hr className="w-100 solid" />
 
