c3747e689c051fd6eca75f116fe6a9744a13d47c
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2023-01-16 11:39:08 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import Index from "./routes/Index";
+import Index from "./routes";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../src/styles/App.css"
 import "../src/styles/Index.css";
