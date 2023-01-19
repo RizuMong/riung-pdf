00ecbcd32ddbc568106a1662589097239842884c
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2023-01-19 17:32:55 +0700

    update

--- src/pages/pdf-1/LogBookSectionReport.jsx
+++ src/pages/pdf-1/LogBookSectionReport.jsx
@@ -18,7 +18,7 @@ const LogBookSectionReport = ({}) => {
   const fetchData = async () => {
     axios
       .post(
-        import.meta.env.VITE_LOGBOOK_URL,
+        "https://gateway.jojonomic.com/v1/nocode/api/rios/generate-pdf/logbook",
         {
           id_logbook: queryParams.get("id_logbook"),
         }
