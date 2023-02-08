28e6c063198688b2bfb16427bd0570041d74b25e
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2023-02-08 17:01:33 +0700

    update ui

--- src/pages/pdf-2/BeritaAcaraPeledakan.jsx
+++ src/pages/pdf-2/BeritaAcaraPeledakan.jsx
@@ -41,30 +41,88 @@ const BeritaAcaraPeledakan = () => {
                   </th>
                 </thead>
               </table>
-              <hr className="w-100 solid"/>
+              <hr className="w-100 solid" />
 
               {/* Header Content */}
               <div className="mb-2">
                 <p>Telah dilaksanakan peledakan,</p>
-                <p>Dilokasi:<span className="text-black">Pit East Kawi – Ekspose Seam 5</span></p>
-                <p>Pada tanggal: <span className="text-black"> 8 - 11- 2022 </span></p>
-                <p>Pada jam: <span className="text-black">12:30 WIB</span></p>
-                <p>Nama juru ledak: <span className="text-black">Heri. P</span></p>
+                <p>
+                  Dilokasi:
+                  <span className="text-black">
+                    Pit East Kawi – Ekspose Seam 5
+                  </span>
+                </p>
+                <p>
+                  Pada tanggal:{" "}
+                  <span className="text-black"> 8 - 11- 2022 </span>
+                </p>
+                <p>
+                  Pada jam: <span className="text-black">12:30 WIB</span>
+                </p>
+                <p>
+                  Nama juru ledak: <span className="text-black">Heri. P</span>
+                </p>
               </div>
 
               <Table responsive bordered>
                 <thead>
                   <tr>
-                    <th rowSpan={2} width="3%" className="text-center align-middle fw-normal">No</th>
-                    <th rowSpan={2} width="30%" className="text-center align-middle fw-normal">Subjek</th>
-                    <th rowSpan={2} width="5%" className="text-center align-middle fw-normal">Satuan</th>
-                    <th rowSpan={2} width="10%" className="text-center align-middle fw-normal">Pelaksanaan</th>
-                    <th colSpan={2} width="30%" className="text-center align-middle fw-normal">Penyimpangan Rencana</th>
-                    <th rowSpan={2} width="22" className="text-center align-middle fw-normal">Alasan Penyimpangan</th>
+                    <th
+                      rowSpan={2}
+                      width="3%"
+                      className="text-center align-middle fw-normal"
+                    >
+                      No
+                    </th>
+                    <th
+                      rowSpan={2}
+                      width="30%"
+                      className="text-center align-middle fw-normal"
+                    >
+                      Subjek
+                    </th>
+                    <th
+                      rowSpan={2}
+                      width="5%"
+                      className="text-center align-middle fw-normal"
+                    >
+                      Satuan
+                    </th>
+                    <th
+                      rowSpan={2}
+                      width="10%"
+                      className="text-center align-middle fw-normal"
+                    >
+                      Pelaksanaan
+                    </th>
+                    <th
+                      colSpan={2}
+                      width="30%"
+                      className="text-center align-middle fw-normal"
+                    >
+                      Penyimpangan Rencana
+                    </th>
+                    <th
+                      rowSpan={2}
+                      width="22"
+                      className="text-center align-middle fw-normal"
+                    >
+                      Alasan Penyimpangan
+                    </th>
                   </tr>
                   <tr>
-                    <th width="11%" className="text-center align-middle fw-normal">Ditambah</th>
-                    <th width="11%" className="text-center align-middle fw-normal">Dikembalikan</th>
+                    <th
+                      width="11%"
+                      className="text-center align-middle fw-normal"
+                    >
+                      Ditambah
+                    </th>
+                    <th
+                      width="11%"
+                      className="text-center align-middle fw-normal"
+                    >
+                      Dikembalikan
+                    </th>
                   </tr>
                 </thead>
                 <tbody className="text-center">
@@ -106,7 +164,9 @@ const BeritaAcaraPeledakan = () => {
                   </tr>
                   <tr>
                     <td>5</td>
-                    <td className="text-start">Electric Detonator No. 8 Instantaneous</td>
+                    <td className="text-start">
+                      Electric Detonator No. 8 Instantaneous
+                    </td>
                     <td>Pcs</td>
                     <td className="text-black">1</td>
                     <td></td>
@@ -129,6 +189,7 @@ const BeritaAcaraPeledakan = () => {
                     <td className="text-start">
                       <p>MS Connector:</p>
                       <p>EXEL TLD 25 …. MS</p>
+                      <p>EXEL TLD 42 .. MS</p>
                     </td>
                     <td>EA</td>
                     <td className="text-black">20</td>
@@ -167,6 +228,15 @@ const BeritaAcaraPeledakan = () => {
                     <td></td>
                     <td></td>
                   </tr>
+                  <tr>
+                    <td>8</td>
+                    <td className="text-start">Power Gel</td>
+                    <td>kg</td>
+                    <td className="text-black">1</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
                 </tbody>
               </Table>
 
@@ -179,8 +249,12 @@ const BeritaAcaraPeledakan = () => {
                       <p>JURU LEDAK</p>
                       <p className="mt-5">(Heri. P)</p>
                     </th>
-                    <th className="fw-normal" colSpan={3}>Diketahui oleh,</th>
-                    <th className="fw-normal" colSpan={3}>Disetujui oleh,</th>
+                    <th className="fw-normal" colSpan={3}>
+                      Diketahui oleh,
+                    </th>
+                    <th className="fw-normal" colSpan={3}>
+                      Disetujui oleh,
+                    </th>
                   </tr>
                   <tr className="text-center align-middle">
                     <th className="fw-normal">JABATAN</th>
@@ -197,7 +271,7 @@ const BeritaAcaraPeledakan = () => {
                     <th className="fw-normal">Kepala Teknik Tamban</th>
                     <th className="fw-normal"></th>
                     <th className="fw-normal"></th>
-                  </tr> 
+                  </tr>
                   <tr className="text-center align-middle">
                     <th className="fw-normal">SATPAM</th>
                     <th className="fw-normal"></th>
@@ -205,7 +279,7 @@ const BeritaAcaraPeledakan = () => {
                     <th className="fw-normal">Kepala Gudang Handak</th>
                     <th className="fw-normal"></th>
                     <th className="fw-normal"></th>
-                  </tr> 
+                  </tr>
                   <tr className="text-center align-middle">
                     <th className="fw-normal"></th>
                     <th className="fw-normal"></th>
@@ -213,7 +287,7 @@ const BeritaAcaraPeledakan = () => {
                     <th className="fw-normal">Drill & Blast Group Leader</th>
                     <th className="fw-normal"></th>
                     <th className="fw-normal"></th>
-                  </tr> 
+                  </tr>
                 </thead>
               </Table>
             </div>
