858a2903114c7a3589bf161e0248cb040129e56a
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2023-02-26 16:31:46 +0700

    update

--- src/pages/pdf-2/DrillingReport.jsx
+++ src/pages/pdf-2/DrillingReport.jsx
@@ -150,8 +150,11 @@ const DrillingReport = () => {
       //     aria-hidden="true"
       //   ></div>
       // </div>
-      <div class="text-center pt-5">
-        <div class="spinner-border text-warning" role="status">
+      <div class="text-center pt-5 position-relative">
+        <div
+          class="spinner-border text-warning position-absolute top-50 start-50 translate-middle"
+          role="status"
+        >
           <span class="visually-hidden">Loading...</span>
         </div>
       </div>
