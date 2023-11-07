fb0de0aec5818b58c43b227e8cc8e7d6887ff504
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2023-11-07 11:06:57 +0700

    update posisition

--- src/pages/pdf-1/LogBookSectionReport.jsx
+++ src/pages/pdf-1/LogBookSectionReport.jsx
@@ -65,8 +65,8 @@ const LogBookSectionReport = ({}) => {
   return (
     <div className="container-fluid">
       <div>
-        <div className="border border-3 border-dark">
-          <div className="p-1">
+        <div>
+          <div>
             <div className="row align-items-center">
               {/* Header Table */}
               <Table responsive bordered className="table">
@@ -75,39 +75,37 @@ const LogBookSectionReport = ({}) => {
                     {" "}
                     <div className="d-flex gap-3 mb-3">
                       <img
-                        className="pl-2"
+                        className="pl-2 img-logbook-header"
                         src={LogoRiung}
-                        width="84"
-                        height="40"
                         alt="Logo RIUNG"
                       />
-                      <h5 className="header-side-logbook fw-bold text-lg">
+                      <h5 className="text-table-logbook fw-bold">
                         PT Riung Mitra Lestari Production Department Jobsite:{" "}
                         {datas.jobsite}
                       </h5>
                     </div>
                   </th>
-                  <div className="vr" style={{ height: 70 }}></div>
+                  <div className="vr height-header-logbook"></div>
                   <th className="col-6">
-                    <h1 className="header-report-production mb-4 fw-semibold">
+                    <h4 className="header-report-logbook mb-4 fw-semibold">
                       LOG BOOK SECTION HEAD REPORT
-                    </h1>
+                    </h4>
                   </th>
-                  <div className="vr" style={{ height: 70 }}></div>
+                  <div className="vr height-header-logbook"></div>
                   <th className="col-3">
-                    <div className="mb-1">
-                      <p className="px-2 fw-normal align-middle text-alat">
+                    <div>
+                      <p className="px-1 fw-normal align-middle text-alat text-table-logbook">
                         No. Dokumen:{" "}
-                        <span className="fw-semibold fst-italic">
+                        <span className="fw-semibold fst-italic text-table-logbook">
                           FRM-OPR - 006
                         </span>
                       </p>
                       <hr className="w-100" />
-                      <p className="px-2  fw-normal align-middle text-alat">
+                      <p className="px-1 fw-normal align-middle text-table-logbook">
                         Hari / Tanggal: {datas.tanggal}
                       </p>
                       <hr className="w-100" />
-                      <p className="px-2 fw-normal align-middle text-alat">
+                      <p className="px-1 fw-normal align-middle text-table-logbook">
                         Halaman: 1{" "}
                       </p>
                       <hr className="w-100" />
@@ -116,44 +114,44 @@ const LogBookSectionReport = ({}) => {
                 </thead>
               </Table>
             </div>
-            <hr className="w-100 solid" />
+            {/* <hr className="w-100 solid" /> */}
 
             {/* Table Content */}
-            <div className="table-responsive">
-              <table className="table table-bordered caption-top mt-2">
+            <div className="table-responsive" >
+              <table className="table table-bordered">
                 <thead className="text-center">
                   <tr>
-                    <th width="3%" scope="col" className="text-table-logbook align-middle">
+                    <th width="2%" className="text-table-logbook align-middle">
                       NO
                     </th>
-                    <th width="10%" scope="col" className="text-table-logbook align-middle">
+                    <th width="8%" className="text-table-logbook align-middle">
                       LOKASI
                     </th>
-                    <th width="5%" scope="col" className="text-table-logbook align-middle">
+                    <th width="5%" className="text-table-logbook align-middle">
                       C/N HAULER
                     </th>
-                    <th width="5%" scope="col" className="text-table-logbook align-middle">
+                    <th width="5%" className="text-table-logbook align-middle">
                       C/N LOADER
                     </th>
-                    <th width="5%" scope="col" className="text-table-logbook align-middle">
+                    <th width="5%" className="text-table-logbook align-middle">
                       C/N SUPPORT
                     </th>
-                    <th width="22%" scope="col" className="text-table-logbook align-middle">
+                    <th width="20%" className="text-table-logbook align-middle">
                       AKTIVITAS
                     </th>
-                    <th width="14%" scope="col" className="text-table-logbook align-middle">
+                    <th width="14%" className="text-table-logbook align-middle">
                       SKETSA
                     </th>
-                    <th width="5%" scope="col" className="text-table-logbook align-middle">
+                    <th width="5%" className="text-table-logbook align-middle">
                       STATUS PKH
                     </th>
-                    <th width="15%" scope="col" className="text-table-logbook align-middle">
+                    <th width="14%" className="text-table-logbook align-middle">
                       PROBLEM
                     </th>
-                    <th width="10%" scope="col" className="text-table-logbook align-middle">
+                    <th width="10%" className="text-table-logbook align-middle">
                       CORRECTIVE ACTION
                     </th>
-                    <th width="5%" scope="col" className="text-table-logbook align-middle">
+                    <th width="5%" className="text-table-logbook align-middle">
                       PARAF
                     </th>
                   </tr>
@@ -209,54 +207,54 @@ const LogBookSectionReport = ({}) => {
                     <th
                       width="15%"
                       scope="col"
-                      className="header-side-logbook table-dark align-middle"
+                      className="text-table-logbook table-dark align-middle"
                     >
                       UNIT BREAK DOWN
                     </th>
                     <th
                       width="15%"
                       scope="col"
-                      className="fs-6 table-dark align-middle"
+                      className="table-dark align-middle text-table-logbook"
                     >
                       UNIT STB
                     </th>
-                    <th width="70%" scope="col" className="header-side-logbook align-middle">
+                    <th width="70%" scope="col" className="text-table-logbook align-middle">
                       CATATAN
                     </th>
                   </tr>
                 </thead>
                 <tbody className="text-center">
-                  <td className="text-sm text-table-logbook fw-normal border border-1 align-middle">
+                  <td align="justify" className="px-1 text-sm text-table-logbook fw-normal border border-1 align-middle">
                     {datas?.unit_breakdown}
                   </td>
-                  <td className="text-sm text-table-logbook fw-normal border border-1 align-middle">
+                  <td align="justify" className="px-1 text-sm text-table-logbook fw-normal border border-1 align-middle">
                     {datas?.unit_stb}
                   </td>
-                  <td className="text-sm text-table-logbook fw-normal border border-1 align-middle">
+                  <td align="justify" className="px-1 text-sm text-table-logbook fw-normal border border-1 align-middle">
                     {datas?.catatan}
                   </td>
                 </tbody>
               </Table>
             </div>
 
-            <hr className="w-100 solid m-0" />
+            {/* <hr className="w-100 solid m-0" /> */}
 
             {/* Content Bottom */}
             <div className="container px-5 pt-3">
               <div className="row align-items-center">
-                <div className="col-4 text-center gap-5">
+                <div className="col-4 text-center text-table-logbook gap-5">
                   <p className="fw-bold">Diserahkan Oleh,</p>
                   <img src={ttd?.ttd_diserahkan} width="100" />
                   <p className="fw-bold">({datas?.diserahkan_oleh})</p>
                   <p className="fw-bold">Section Shift 1</p>
                 </div>
-                <div className="col-4 text-center gap-5">
+                <div className="col-4 text-center text-table-logbook gap-5">
                   <p className="fw-bold">Diterima Oleh,</p>
                   <img src={ttd?.ttd_diterima} width="100" />
                   <p className="fw-bold">({datas?.diterima_oleh})</p>
                   <p className="fw-bold">Section Shift 2</p>
                 </div>
-                <div className="col-4 text-center">
+                <div className="col-4 text-center text-table-logbook">
                   <p className="fw-bold">Diketahui,</p>
                   <img src={ttd?.ttd_diketahui} width="100" />
                   <p className="fw-bold">({datas?.diketahui})</p>
@@ -267,13 +265,13 @@ const LogBookSectionReport = ({}) => {
           </div>
         </div>
       </div>
-      {sketsa &&
+      {/* {sketsa &&
         sketsa.map((item, index) => (
           <div key={index}>
             <img width="1000" src={item?.url} alt={item?.name} />
             <br />
           </div>
-        ))}
+        ))} */}
     </div>
   );
 };
